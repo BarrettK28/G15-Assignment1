0e4410555ab8c5a4210af0dae3b5d3c59161dd88
I-Alexander-I <109490990+I-Alexander-I@users.noreply.github.com>
2026-02-17 19:33:38 -0600

    inital stuff

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,4 +1,79 @@
+//aj
+
+//Represents a reservation for one student in one room at one time slot.
 
 public class Reservation {
 
+	//declaring variables
+	private int id;
+	private Room room;
+	private String studentName;
+	private TimeSlot timeSlot;
+	private boolean canceled;
+	private boolean checkedIn;
+	
+	//constructor
+	public Reservation(int id, Room room, String studentName, TimeSlot timeSlot, boolean canceled, boolean checkedIn) {
+		// ID must be positive		
+		if (id<=0)
+		{
+			throw new IllegalArgumentException();
+		}
+		//studentName must not be null or blank
+		if (studentName == null || studentName == " ")
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		//cannot be null
+		if (timeSlot == null || room == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		//cannot be canceled and checked in
+		if (checkedIn == true && canceled == true)
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		//initializing after enforcing invariants 
+		this.id = id;
+		this.room = room;
+		this.studentName = studentName;
+		this.timeSlot = timeSlot;
+		this.canceled = canceled;
+		this.checkedIn = checkedIn;
+		
+	}
+	
+	//methods
+	
+	//marks the reservation canceled if allowed
+	public void cancel()
+	{
+		if (this.canceled == true)
+		{
+			System.out.println("Reservation has already been cancelled.");
+		}
+		else if(this.checkedIn == false)
+		{
+			System.out.println("Reservation was not checkedIn, so cannot cancel Reservation.");
+		}
+		
+		//if was checked in and was not previously cancelled
+		this.canceled = true;
+		this.checkedIn = false;
+		System.out.println("Reservation has been cancelled.");
+	}
+	
+	//marks the reservation checked in if allowed
+	public void checkIn()
+	{
+		
+	}
+	
+	//Methods that answer questions such as isActive() or canCheckIn() are encouraged
+	
+	//toString() prints a useful line including id, student, room, time slot, and status
 }
--- src/ReservationBook.java
+++ src/ReservationBook.java
@@ -1,4 +1,12 @@
+//aj
+
+// This class stores reservations and owns them. It should not coordinate the whole application, it
+// should manage reservation storage and basic queries.
+
+//Constraints: You must store reservations using a plain Java array
 
 public class ReservationBook {
-
+	
+	
+	
 }
